bc7bf6e521da23bc142a9b12c6599a4ad28d270a
Chuang Jie Bo <bochuangjie@gmail.com>
2022-11-16 17:55:52 +0800

    a simple m3u8 movie downloader

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/tonybobo/duoduo-downloader
+
+go 1.19
+
+require github.com/PuerkitoBio/goquery v1.8.0
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/canhlinh/hlsdl v0.0.0-20210810121645-29f1ce6d3208 // indirect
+	github.com/grafov/m3u8 v0.11.1 // indirect
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	gopkg.in/cheggaaa/pb.v1 v1.0.28 // indirect
+)
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/canhlinh/hlsdl"
+)
+
+func main() {
+	var text string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please Provide The Exact Movie Name")
+	fmt.Println("---------------------")
+	for {
+		text, _ = reader.ReadString('\n')
+		if text != "" {
+			break
+		}
+	}
+	list, _ := getM3u8Url(strings.Trim(text, "\r\n"))
+	fmt.Print(list)
+	hlsDL := hlsdl.New(list[0], nil, "download", 10, true)
+	result, err := retry(hlsDL)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(result)
+}
+
+func retry(hlsDL *hlsdl.HlsDl) (string, error) {
+	for i := 0; i < 10; i++ {
+		filepath, err := hlsDL.Download()
+		if i > 0 {
+			os.RemoveAll("download")
+			fmt.Printf("retrying after error %s", err)
+			time.Sleep(time.Second * 5)
+		}
+		if err == nil {
+			return fmt.Sprintf("video has been downloaded%s", filepath), nil
+		}
+	}
+	return "", errors.New("unable to download video after fifth tries")
+}
+
+func getM3u8Url(text string) ([]string, error) {
+	var list []string
+	url := fmt.Sprintf("https://ddzyz1.com/vodsearch/-------------.html?wd=%s&submit=search", text)
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	title, exist := doc.Find(".xing_vb ul .xing_vb4 a").Attr("href")
+	if !exist {
+		fmt.Printf("No Movie with such name has been found")
+		return list, errors.New("no movie with such name has been found")
+	}
+
+	videoUrl := fmt.Sprintf("https://ddzyz1.com%s", title)
+	videoRes, err := http.Get(videoUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer videoRes.Body.Close()
+	videoDoc, err := goquery.NewDocumentFromReader(videoRes.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	untrimString := videoDoc.Find(".vodplayinfo ul li").Text()
+	m3u8 := strings.Split(strings.Split(untrimString, "$")[1], " ")[0]
+	if m3u8 == "" {
+		fmt.Print("no available files")
+		return list, errors.New("no available files")
+	}
+	list = append(list, m3u8)
+	return list, nil
+}
